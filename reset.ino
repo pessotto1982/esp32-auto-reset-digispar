f642de91c85ead3fc6500a82c123dc2553d1bb3b
Stefano Pessotto <47522827+pessotto1982@users.noreply.github.com>
2026-04-08 18:16:05 +0200

    Create reset.ino

---
+++ reset.ino
@@ -0,0 +1,22 @@
+#define RST_PIN 0  // P5 = PB0 su Digispark
+#define LED_PIN 1
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(RST_PIN, INPUT);  // Alta impedenza
+  digitalWrite(LED_PIN, LOW);
+}
+
+void loop() {
+  // Reset ESP32
+  pinMode(RST_PIN, OUTPUT);
+  digitalWrite(RST_PIN, LOW);  // EN → GND
+  digitalWrite(LED_PIN, HIGH);
+  delay(300);
+
+  // Release
+  pinMode(RST_PIN, INPUT);     // EN risale
+  digitalWrite(LED_PIN, LOW);
+  
+  delay(43200000); // 12 ore
+}
